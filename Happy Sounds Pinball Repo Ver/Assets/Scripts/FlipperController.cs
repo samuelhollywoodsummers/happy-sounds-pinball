9d374c2afcee0c01e5ddb0d57eecd8c31179ae1a
Samuel Hollywood-Summers <45264945+samuelhollywoodsummers@users.noreply.github.com>
2020-12-05 15:41:42 +0000

    Right Flipper responding
    Left not

---
+++ Happy Sounds Pinball Repo Ver/Assets/Scripts/FlipperController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperController : MonoBehaviour
+{
+    public bool Flipper;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Flipper && Input.GetKey(KeyCode.RightControl)) || (!Flipper && Input.GetKey(KeyCode.LeftControl)))
+        {
+            GetComponent<HingeJoint2D>().useMotor = true;
+        }
+        else
+        {
+            GetComponent<HingeJoint2D>().useMotor = false;
+        }
+    }
+}
