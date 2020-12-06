9a474be84eb6f1340182821b28df314e92479b15
Samuel Hollywood-Summers <45264945+samuelhollywoodsummers@users.noreply.github.com>
2020-12-06 18:58:47 +0000

    Gameplay Loop
    Destructible obstacle added that is removed with space to begin playing. Collision with sprite shape below flippers causes scene to reload/reset.

---
+++ Happy Sounds Pinball Repo Ver/Assets/LauncherController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherController : MonoBehaviour
+{
+       //movement speed in units per second
+    private float movementSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown("space"))
+        {
+            GetComponent<SliderJoint2D>().useMotor = true;
+        }
+        else
+        {
+            GetComponent<SliderJoint2D>().useMotor = false;
+        }
+   
+    }
+}
